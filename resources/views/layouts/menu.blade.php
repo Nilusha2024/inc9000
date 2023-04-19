1796845042797697c4104ac346e5d9ec8e19e694
thiran <thiran.naleendra@rabbit.lk>
2023-04-19 10:17:19 +0530

    added side nav bar and home page

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,40 @@
+{{-- filter these menus --}}
+
+<li class="nav-item">
+
+    {{-- Home --}}
+    <a href="#" class="nav-link ">
+        <i class="nav-icon fas fa-home"></i>
+        <p>Home</p>
+    </a>
+
+    <a href="genaral" class="nav-link">
+        <i class="nav-icon fas fa-user"></i>
+        <p>General</p>
+    </a>
+
+    <a href="test" class="nav-link">
+        <i class="nav-icon fas fa-user"></i>
+        <p>test </p>
+    </a>
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-user"></i>
+        <p>test</p>
+    </a>
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-user"></i>
+        <p>test</p>
+    </a>
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-user"></i>
+        <p>test</p>
+    </a>
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-user"></i>
+        <p>test</p>
+    </a>
+
+
+
+
+</li>
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,1 +1,3 @@
-<h1>Test</h1>
+@extends('layouts.app')
+
+@section('content')
